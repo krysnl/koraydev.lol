e6fc0e2a5374f2b8ad20bc7fe75ed358d82e6963
dtksenel <koray.senel@digiturk.com.tr>
2023-10-04 23:57:31 +0300

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -10,6 +10,17 @@ app.get('/', function(req, res) {
 server.on('request', app);
 server.listen(3000, function(){ console.log('server started on port 3000'); });
 
+process.on('SIGINT', () => {
+  console.log('Received SIGINT.  Shutting down...');
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 /** Websocket **/
 const WebSocketServer = require('ws').Server;
 
@@ -25,6 +36,11 @@ wss.on('connection', function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send('welcome!');
   }
+
+  db.run(`
+    INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now')) 
+  `);
 
   ws.on('close', function close() {
     wss.broadcast(`Current visitors: ${wss.clients.size}`);
@@ -48,3 +64,28 @@ wss.broadcast = function broadcast(data) {
   });
 };
 /** End Websocket **/
+
+/** Begin Database **/
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize( () => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `)
+});
+
+function getCounts() {
+  db.each('SELECT * FROM visitors', (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log('Closing database connection');
+  db.close();
+}
